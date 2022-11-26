e2400774e04ad4230315dd95737349da2765b2bb
Valeentin87 <valeentin87@list.ru>
2022-11-26 18:48:54 +0300

    добавили программу, не позволяющую разделить на 0

---
+++ src/main/java/ForDemo.java
@@ -0,0 +1,10 @@
+/* программа, демонстрирующая работу цикла for*/
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+        for(count=0;count<5;count++) {
+            System.out.println("значение переменной count цикла равно "+ count);
+        }
+        System.out.println("Готово!");
+    }
+}
