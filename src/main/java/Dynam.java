9075d474e7e6a84f24cda216107a774eb7fcf71b
Valeentin87 <valeentin87@list.ru>
2022-11-27 00:04:51 +0300

    разобрали операции и преобразования типов

---
+++ src/main/java/Dynam.java
@@ -0,0 +1,8 @@
+public class Dynam {
+    public static void main(String[] args) {
+        double radius = 5;
+        double height = 4;
+        double volume = 3.1415*radius*radius*height; // динамическая типизация переменной double
+        System.out.println("Объем цилиндра равен " + volume);
+    }
+}
---
+++ src/main/java/Sound.java
@@ -0,0 +1,9 @@
+public class Sound {
+    public static void main(String[] args) {
+        double v = 1100;
+        double s;
+        double t = 7.2;
+        s = v*t;
+        System.out.println("Расстояние до источника звука, возникающего при вспышке молнии равно " + s + " футов");
+    }
+}
