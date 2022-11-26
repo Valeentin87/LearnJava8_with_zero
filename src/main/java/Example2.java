30cfb7c0bb5aef836eff6f3eb3e94fd4182d53ad
Valeentin87 <valeentin87@list.ru>
2022-11-26 14:39:46 +0300

    добавили пример Example, Example2

---
+++ src/main/java/Example2.java
@@ -0,0 +1,14 @@
+/* объявление переменных и присваивание им значений
+вывод на экран с неявной типизацией переменных
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+        var1 = 1024;
+        var2 = var1/2;
+        System.out.println("Переменная var1 равна " + var1);
+        System.out.println("Переменная var2 равна равна var1/2");
+        System.out.println(var2);
+    }
+}
